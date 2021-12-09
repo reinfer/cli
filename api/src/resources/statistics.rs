003fa6cbe52f2ea219467adabd518ecba31cc0b8
Joe Prosser <joe.prosser@reinfer.io>
2021-12-09 14:38:02 +0000

    resources: statistics should expect f64 not usize

--- api/src/resources/statistics.rs
+++ api/src/resources/statistics.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub(crate) struct GetResponse {
     pub statistics: Statistics,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct Statistics {
-    pub num_comments: usize,
+    pub num_comments: f64,
 }
