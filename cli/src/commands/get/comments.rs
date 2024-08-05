7b3a80c987e0a6d885b5cffecc1ce4c403bcbaa9
Joe Prosser <joe.prosser@uipath.com>
2024-08-05 16:28:56 +0100

    fix it more

--- cli/src/commands/get/comments.rs
+++ cli/src/commands/get/comments.rs
@@ -124,7 +124,7 @@ pub struct GetManyCommentsArgs {
     include_attachment_content: Option<bool>,
 
     #[structopt(long = "--only-with-attachments")]
-    /// Whether to return comments with attachment metadata
+    /// Whether to only return comments with attachment metadata
     only_with_attachments: Option<bool>,
 }
 
