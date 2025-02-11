38bc6f23c3a4ca0ce864d1cd96425c672984c3f4
joe-prosser <joe.prosser@uipath.com>
2025-02-11 10:54:10 +0000

    chore(tests): increase timeout for test project (#339)

--- cli/tests/test_projects.rs
+++ cli/tests/test_projects.rs
@@ -34,7 +34,7 @@ impl TestProject {
                 break;
             }
 
-            if start_time.elapsed().as_secs() > 30 {
+            if start_time.elapsed().as_secs() > 120 {
                 panic!("Timed out waiting for project to be created");
             }
 
